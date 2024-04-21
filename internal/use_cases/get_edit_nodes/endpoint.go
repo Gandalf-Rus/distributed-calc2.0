beb16c46854703293e755e971153a2e6db3b00a4
Khairullin Ruslan <ruslanrbb8@gmail.com>
2024-04-21 14:23:47 +0300

    proto file: agentid changed to string

--- internal/use_cases/get_edit_nodes/endpoint.go
+++ internal/use_cases/get_edit_nodes/endpoint.go
@@ -1,7 +1,14 @@
 package geteditnodes
 
 import (
+	"context"
+
+	"github.com/Gandalf-Rus/distributed-calc2.0/internal/agent"
+	"github.com/Gandalf-Rus/distributed-calc2.0/internal/errors"
+	grpcconversion "github.com/Gandalf-Rus/distributed-calc2.0/internal/grpc_conversion"
+	"github.com/Gandalf-Rus/distributed-calc2.0/internal/logger"
 	pb "github.com/Gandalf-Rus/distributed-calc2.0/proto"
+	"github.com/golang/protobuf/ptypes/empty"
 )
 
 func NewServer(repo repo) *Server {
@@ -15,38 +22,38 @@ type Server struct {
 	repo                 repo
 }
 
-// func (s *Server) GetNodes(ctx context.Context, in *pb.GetNodesRequest) (*pb.GetNodesResponse, error) {
-// 	nodes, err := s.repo.EditNodesStatusAndGetReadyNodes(int(in.AgentId), int(in.FreeWorkers))
-// 	if err != nil {
-// 		logger.Slogger.Error(err)
-// 		return nil, errors.ErrInternalServerError
-// 	}
+func (s *Server) GetNodes(ctx context.Context, in *pb.GetNodesRequest) (*pb.GetNodesResponse, error) {
+	nodes, err := s.repo.EditNodesStatusAndGetReadyNodes(in.AgentId, int(in.FreeWorkers))
+	if err != nil {
+		logger.Slogger.Error(err)
+		return nil, errors.ErrInternalServerError
+	}
 
-// 	var protoNodes []*pb.Node
-// 	var protoNode *pb.Node
-// 	for _, node := range nodes {
-// 		protoNode, err = grpcconversion.NodeToGrpcNode(node)
-// 		logger.Slogger.Info(protoNode)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		protoNodes = append(protoNodes, protoNode)
-// 	}
+	var protoNodes []*pb.Node
+	var protoNode *pb.Node
+	for _, node := range nodes {
+		protoNode, err = grpcconversion.NodeToGrpcNode(node)
+		logger.Slogger.Info(protoNode)
+		if err != nil {
+			return nil, err
+		}
+		protoNodes = append(protoNodes, protoNode)
+	}
 
-// 	return &pb.GetNodesResponse{
-// 		Nodes: protoNodes,
-// 	}, nil
-// }
+	return &pb.GetNodesResponse{
+		Nodes: protoNodes,
+	}, nil
+}
 
-// func (s *Server) TakeHeartBeat(ctx context.Context, in *pb.GetNodesRequest) (*empty.Empty, error) {
-// 	if agent.IsAgent(int(in.AgentId)) {
-// 		agent.TakeHeartBeat(int(in.AgentId))
-// 	} else {
-// 		agent.RegistrateAgent(int(in.AgentId))
-// 	}
-// 	return nil, nil
-// }
+func (s *Server) TakeHeartBeat(ctx context.Context, in *pb.GetNodesRequest) (*empty.Empty, error) {
+	if agent.IsAgent(in.AgentId) {
+		agent.TakeHeartBeat(in.AgentId)
+	} else {
+		agent.RegistrateAgent(in.AgentId)
+	}
+	return nil, nil
+}
 
-// func (s *Server) EditNode(ctx context.Context, in *pb.EditNodeRequest) (*empty.Empty, error) {
-// 	return nil, nil
-// }
+func (s *Server) EditNode(ctx context.Context, in *pb.EditNodeRequest) (*empty.Empty, error) {
+	return nil, nil
+}
